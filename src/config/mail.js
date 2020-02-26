3e0955efa6246c9981629b089e5e57a4a2cb01ec
Tiago Felipe <tiagofsv95@gmail.com>
2020-02-26 17:55:16 -0300

    Commit config Redis and Mails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '5532da88564eaf',
+    pass: '74c434d291fefb',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
