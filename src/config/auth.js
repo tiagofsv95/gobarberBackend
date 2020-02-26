a0d2aaf1c99ceaeb92682285b72499fba89038ff
Tiago Felipe <tiagofsv95@gmail.com>
2020-02-26 19:24:56 -0300

    Backend done

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '828099fa74a674ecdf964db818b2f37e',
+  secret: process.env.APP_SECRET, // '828099fa74a674ecdf964db818b2f37e',
   expiresIn: '7d',
 };
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: process.env.SENTRY_DSN, // 'https://c3d54f38f79d46928b77442678b82b72@sentry.io/3004078',
+};
